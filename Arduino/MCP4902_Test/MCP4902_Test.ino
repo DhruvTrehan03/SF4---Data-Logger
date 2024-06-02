fdb0121d083b4005cc8d5beba21dabbb4418d5e0
DhruvTrehan03 <98122416+DhruvTrehan03@users.noreply.github.com>
2024-06-02 09:30:42 +0100

    TestCode

--- Arduino/MCP4902_Test/MCP4902_Test.ino
+++ Arduino/MCP4902_Test/MCP4902_Test.ino
@@ -8,13 +8,10 @@
 
 #include <SPI.h>         // Remember this line!
 #include <DAC_MCP49xx.h>
+#include <analogWave.h>
 
 #define SS_PIN 10 // The Arduino pin used for the slave select / chip select
 #define LDAC_PIN 7 // The Arduino pin used for the LDAC (output synchronization) feature
-#define VA A0
-#define VB A1
-
-float i =0;
 
 // Set up the DAC. 
 // First argument: DAC model (MCP4902, MCP4912, MCP4922)
@@ -22,6 +19,10 @@ float i =0;
 // (The third argument, the LDAC pin, can be left out if not used)
 
 DAC_MCP49xx dac(DAC_MCP49xx::MCP4902, SS_PIN, LDAC_PIN);     //the first argument was changed from MCP49x2(original code) to MCP4902 to apply to this DAC
+analogWave wave(DAC_MCP49xx);   // Create an instance of the analogWave class, using the DAC pin
+
+int freq = 10;  // in hertz, change accordingly
+float i = 0;
 
 void setup() {
 
@@ -37,7 +38,7 @@ void setup() {
   // Pull the LDAC pin low automatically, to synchronize output
   // This is true by default, however.
   dac.setAutomaticallyLatchDual(true);
-
+  wave.
 }
 
  
